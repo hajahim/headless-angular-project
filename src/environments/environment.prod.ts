9de1950dd17933f5f883da4f604a1b2e533b6db1
Ravoatra Mihaja RAJAONARIVELO <hajahim15@gmail.com>
2022-01-31 18:09:58 +0300

    #11 | Adding configuration to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfiguration: {
+    apiKey: 'AIzaSyDqPst1JL0EWfa9h-ZYVM2p7-QNXFARLTM',
+    authDomain: 'headless-angular.firebaseapp.com',
+    databaseURL: 'https://headless-angular-default-rtdb.firebaseio.com',
+    projectId: 'headless-angular',
+    storageBucket: 'headless-angular.appspot.com',
+    messagingSenderId: '60892068353'
+  }
 };
